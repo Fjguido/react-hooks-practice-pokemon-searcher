f5d514d793b0f3654a653ed97403cfcb67e0cc53
Francis Guido <fjguido97@gmail.com>
2022-02-06 21:46:51 -0500

    commit tree aad71d968fa4ab6fa021a1cd359df84c709de0db

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -1,29 +1,66 @@
-import React from "react";
+import React, {useState} from "react";
 import { Form } from "semantic-ui-react";
 
-function PokemonForm() {
+function PokemonForm(addPokemon) {
+
+  const[formPokemon, setFormPokemon] = useState({
+    name: '', 
+    hp: '',
+    front: '',
+    back: '',
+
+  })
+
+  function handleChange(e) {
+    setFormPokemon({...formPokemon, [e.target.name]: e.target.value,
+    });
+
+  }
+
+  function handleSubmit(e){
+    e.preventDefault()
+    const newPokemon ={
+      name: formPokemon.name,
+      hp: formPokemon.hp,
+      sprites:{
+        front: formPokemon.frontUrl,
+        back: formPokemon.backUrl,
+      }
+      
+      
+    };
+
+    fetch("http://localhost:3001/pokemon", {
+      method: "POST",
+      headers: {'Content-Type' : 'application/json'}, body: JSON.stringify(newPokemon)
+    })
+    .then(resp => resp.json())
+    .then(addPokemon)
+  }
   return (
     <div>
       <h3>Add a Pokemon!</h3>
       <Form
-        onSubmit={() => {
-          console.log("submitting form...");
-        }}
+        onSubmit={handleSubmit}
       >
         <Form.Group widths="equal">
-          <Form.Input fluid label="Name" placeholder="Name" name="name" />
-          <Form.Input fluid label="hp" placeholder="hp" name="hp" />
+          <Form.Input fluid label="Name" placeholder="Name" name="name" value={formPokemon.name} onChange={handleChange} />
+          <Form.Input fluid label="hp" placeholder="hp" name="hp" value={formPokemon.hp} onChange={handleChange} />
           <Form.Input
             fluid
             label="Front Image URL"
             placeholder="url"
             name="frontUrl"
+            value={formPokemon.frontUrl}
+            onChange={handleChange}
           />
           <Form.Input
             fluid
             label="Back Image URL"
             placeholder="url"
             name="backUrl"
+            value={formPokemon.backUrl}
+            onChange={handleChange}
           />
         </Form.Group>
         <Form.Button>Submit</Form.Button>
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+  const[pokemon, setPokemon]=useState([])
+  const[search, setSearch]=useState('')
+
+  useEffect(() => {
+    fetch("http://localhost:3001/pokemon")
+    .then((resp) => resp.json())
+    .then((data) => setPokemon(data))
+  }, [])
+
+  const filterPokemon=pokemon.filter((data) => {
+    return data.name.toLowerCase().includes(search.toLowerCase())
+  })
+
+  function addPokemon(newPokemon){
+    setPokemon([...pokemon, newPokemon])
+
+  }
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
-      <PokemonForm />
+      <PokemonForm addPokemon={addPokemon} />
       <br />
-      <Search />
+      <Search setSearch={setSearch} search={search} />
       <br />
-      <PokemonCollection />
+      <PokemonCollection pokemon={filterPokemon}/>
     </Container>
   );
 }
